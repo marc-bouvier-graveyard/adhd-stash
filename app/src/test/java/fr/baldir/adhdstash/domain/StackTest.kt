a33cf7b0ffdf183cfe69c8f61e93e8d4676c16be
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-09-13 19:33:55 +0200

    test(domain): A stash can push and put an element

--- app/src/test/java/fr/baldir/adhdstash/domain/StackTest.kt
+++ app/src/test/java/fr/baldir/adhdstash/domain/StackTest.kt
@@ -1,13 +1,21 @@
 package fr.baldir.adhdstash.domain
 
-import junit.framework.Assert.assertEquals
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.util.*
 
 class StackTest {
 
     @Test
-    fun a_new_stack_is_empty(){
-        assertEquals(Stack<String>().size,0)
+    fun a_new_stash_is_empty() {
+        assertEquals(0, Stack<String>().size)
+    }
+
+    @Test
+    fun when_an_element_is_stashed_the_same_is_popped() {
+        val stash = Stack<String>()
+        stash.push("I was writing a test with TDD")
+        val actual = stash.pop()
+        assertEquals("I was writing a test with TDD", actual)
     }
 }
